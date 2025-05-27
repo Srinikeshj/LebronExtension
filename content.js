029779658193d2cdc3e6cd82183b2d45caadc2ec
Srinikesh Javvaji <Jsrinikesh@gmail.com>
2025-05-27 10:37:06 -0500

    Extension

---
+++ content.js
@@ -0,0 +1,5 @@
+const imgs = document.getElementsByTagName("img");
+
+for (image of imgs){
+  image.src = "https://th.bing.com/th/id/OIP.afaNuDR132bCYK2qIJnQ_QHaEK?w=327&h=184&c=7&r=0&o=5&pid=1.7"
+}
